b8fd6dabfdeacb54c8a1624aaa6ca2e848443e45
ad-swiderek <37022061+ad-swiderek@users.noreply.github.com>
2019-05-26 19:14:25 +0200

    Usuwanie z koszyka

--- app/src/main/java/com/example/adrian/myapplication/CartActivity.java
+++ app/src/main/java/com/example/adrian/myapplication/CartActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -24,7 +25,6 @@ import java.util.ArrayList;
 public class CartActivity extends AppCompatActivity {
     ActivityCartBinding binding;
     private ArrayList<String> listItem;
-    private ArrayList<String> product;
     private ArrayAdapter adapter;
     private ListView listView;
     private TextView totalAmountTV;
@@ -44,7 +44,7 @@ public class CartActivity extends AppCompatActivity {
         offlinePaymentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendTotalAmount();
+                startQRActivity();
 
             }
         });
@@ -63,18 +63,9 @@ public class CartActivity extends AppCompatActivity {
                 pricePerOne = Float.parseFloat(cursor.getString(3)) * Float.parseFloat(cursor.getString(4));
                 fullPrice += pricePerOne;
                 listItem.add(cursor.getString(2) + " | sztuk:  " + cursor.getString(4)
-                        + " | łączna cena: " + pricePerOne + " (" + cursor.getString(3) + " za szt.)");
+                        + " | łączna cena: " + String.format("%.2f", pricePerOne) + " (" + cursor.getString(3) + " za szt.)");
             }
-            totalAmountTV.setText("DO ZAPŁATY: " + String.format("%.2f", fullPrice) + " PLN");
-            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listItem);
-            listView.setAdapter(adapter);
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    adapter.remove(adapter.getItem(position));
-                    //readFromDB();
-                }
-            });
+            displayProducts();
         }
     }
 
@@ -83,12 +74,50 @@ public class CartActivity extends AppCompatActivity {
 
     }
 
-    private void sendTotalAmount() {
+    private void displayProducts() {
+        totalAmountTV.setText("DO ZAPŁATY: " + String.format("%.2f", fullPrice) + " PLN");
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_single_choice, listItem);
+        listView.setAdapter(adapter);
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        /*listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //adapter.remove(adapter.getItem(position));
+                //readFromDB();
+                //SparseBooleanArray sp = listView.getCheckedItemPositions();
+            }
+        });*/
+
+        Button deleteBtn = findViewById(R.id.deleteBtn);
+        //deleteBtn.setEnabled(false);
+
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*SparseBooleanArray checkedItem = listView.getCheckedItemPositions();
+                int itemCount = listView.getCount();
+                for (int i = 0; i < itemCount; i++) {
+                    if (checkedItem.get(i)) {
+                        adapter.remove(listItem.get(i));
+                    }
+
+                    adapter.notifyDataSetChanged();
+                }*/
+                int checked = listView.getCheckedItemPosition();
+                adapter.remove(adapter.getItem(checked));
+            }
+        });
+    }
+
+    private void deleteProduct() {
+        //SparseBooleanArray sp = getListView
+    }
+
+    private void startQRActivity() {
         Intent intent = new Intent(this, QRCodePayment.class);
         String message = String.format("%.2f", fullPrice);
         intent.putExtra(EXTRA_MESSAGE, message);
         ActivityCompat.finishAffinity(this);
-        //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 
